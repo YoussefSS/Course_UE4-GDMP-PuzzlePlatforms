776822c18539f77a9dff81df8d479cb258fd58aa
YoussefSS <joesamir98@gmail.com>
2022-01-24 12:08:05 +0200

    15. Handling Overlap Events In C++

--- Source/PuzzlePlatformsMP/PlatformTrigger.cpp
+++ Source/PuzzlePlatformsMP/PlatformTrigger.cpp
@@ -12,8 +12,9 @@ APlatformTrigger::APlatformTrigger()
 
 	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(FName("TriggerVolume"));
 	if (!ensure(TriggerVolume != nullptr)) return;
-
 	RootComponent = TriggerVolume;
+
+
 }
 
 // Called when the game starts or when spawned
@@ -21,10 +22,24 @@ void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (!ensure(TriggerVolume != nullptr)) return;
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
 void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
