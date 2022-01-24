bda2832fe992db07cdb1877768629e006fbe21d6
YoussefSS <joesamir98@gmail.com>
2022-01-24 12:25:53 +0200

    16. Activating Platforms From Triggers

--- Source/PuzzlePlatformsMP/MovingPlatform.h
+++ Source/PuzzlePlatformsMP/MovingPlatform.h
@@ -29,9 +29,14 @@ public:
 	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
 	FVector TargetLocation;
 
+	void AddActiveTrigger();
+	void RemoveActiveTrigger();
 
 private:
 	FVector GlobalTargetLocation;
 	FVector GlobalStartLocation;
+
+	UPROPERTY(EditAnywhere)
+	int32 ActiveTriggers = 1;
 
 };
